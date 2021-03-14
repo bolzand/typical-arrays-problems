267bc2919eb98f744ee8c4c4f34f4916fe47373d
bolzand <dmitry.bolzan@gmail.com>
2021-03-14 18:54:24 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  }
+  else {
+    return Math.min.apply(null, array);
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  }
+  else {
+    return Math.max.apply(null, array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0){
+    return 0;
+  }
+  function reducer(accumulator, currentValue) {
+    return accumulator + currentValue;
+  }
+  return array.reduce(reducer)/array.length;
 }
